a8c5bbecf6aac74a7021c7a47f2fe2c82d5028fc
Dam's <damien.champeyroux@epita.fr>
2021-07-29 00:20:44 +0200

    feat (search): searching backend usage

--- comments/views.py
+++ comments/views.py
@@ -1,12 +1,19 @@
 # Create your views here.
+from django.db.models import Q
 from django.shortcuts import render
 
 from .models import Comment
 
 
 def search_view(request):
-    obj = Comment.objects.get(id=1)
+    all_comments = Comment.objects.all()
+    word = request.POST['searchname']
+
+    if request.method == "POST" and word:
+        all_comments = all_comments.filter(Q(title__contains=word) | Q(body__contains=word))
+
     context = {
-        'obj': obj
+        'all_comments': all_comments,
+        'word_researched': word,
     }
     return render(request, "search/search.html", context)
